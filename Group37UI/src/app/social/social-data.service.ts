bf8223e0442ee17f2521e90bb1bf4c8193b26471
Utkarsh <uvas7225@uni.sydney.edu.au>
2023-10-12 14:01:51 +1100

    using axios for social and added 5 posts

--- Group37UI/src/app/social/social-data.service.ts
+++ Group37UI/src/app/social/social-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of,catchError } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -8,32 +8,62 @@ import { Observable, of,catchError } from 'rxjs';
 export class SocialDataService {
   private backendApiUrl = 'https://your-backend-api-url'; // Replace with your backend API URL
 
-  constructor(private http: HttpClient) { }
+  constructor() {}
 
   // Fetch user profile from the backend or use dummy data
   getUserProfile(): Observable<any> {
-    return this.http.get<any>(`${this.backendApiUrl}/user-profile`).pipe(
-      catchError(() => {
-        // If the API request fails, provide dummy data
-        return of({ name: 'John Doe', bio: 'This is a sample bio', friends: ['Friend 1', 'Friend 2'] });
-      })
-    );
+    return new Observable<any>((observer:any) => {
+      axios
+        .get(`${this.backendApiUrl}/user-profile`)
+        .then((response) => {
+          observer.next(response.data);
+        })
+        .catch(() => {
+          // If the API request fails, provide dummy data
+          observer.next({ name: 'John Doe', bio: 'This is a sample bio', friends: ['Friend 1', 'Friend 2'] });
+        });
+    });
   }
 
   // Fetch posts from the backend or use dummy data
   getPosts(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.backendApiUrl}/posts`).pipe(
-      catchError(() => {
-        // If the API request fails, provide dummy data
-        return of([
-          {
-            authorName: 'John Doe',
-            authorImage: 'profile-image-url',
-            content: 'This is a sample post content...',
-          },
-          // Add more dummy posts here
-        ]);
-      })
-    );
+    return new Observable<any[]>((observer:any) => {
+      axios
+        .get(`${this.backendApiUrl}/posts`)
+        .then((response) => {
+          observer.next(response.data);
+        })
+        .catch(() => {
+          // If the API request fails, provide dummy data
+          observer.next([
+            {
+              authorName: 'John Doe',
+              authorImage: 'profile-image-url',
+              content: 'This is a sample post content...',
+            },
+            {
+              authorName: 'Jane Smith',
+              authorImage: 'profile-image-url',
+              content: 'Another dummy post...',
+            },
+            {
+              authorName: 'Alice Johnson',
+              authorImage: 'profile-image-url',
+              content: 'Yet another post...',
+            },
+            {
+              authorName: 'Bob Anderson',
+              authorImage: 'profile-image-url',
+              content: 'Sample post by Bob...',
+            },
+            {
+              authorName: 'Eva Davis',
+              authorImage: 'profile-image-url',
+              content: 'A post from Eva...',
+            },
+            // Add more dummy posts here
+          ]);
+        });
+    });
   }
 }
