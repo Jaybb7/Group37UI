7c80796024f62e05e730d197e8e5b6b0aa5d2a86
Taizeen <taizeenmunir18@gmail.com>
2023-10-22 16:35:23 +1100

    fix compilation issues

--- Group37UI/src/app/app-routing.module.ts
+++ Group37UI/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PurposePageComponent } from './purpose-page/purpose-page.component';
-import { MatrimonialDashboardComponent } from './matrimonial-dashboard/matrimonial-dashboard.component';
-import { SocialDashboardComponent } from './social-dashboard/social-dashboard.component';
-import { JobDashboardComponent } from './job-dashboard/job-dashboard.component';
+// import { PurposePageComponent } from './purpose-page/purpose-page.component';
+// import { MatrimonialDashboardComponent } from './matrimonial-dashboard/matrimonial-dashboard.component';
+import { SocialDashboardComponent } from './social/social-dashboard.component';
+// import { JobDashboardComponent } from './job-dashboard/job-dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: PurposePageComponent },
-  { path: 'dashboard/matrimonial', component: MatrimonialDashboardComponent },
+  // { path: '', component: PurposePageComponent },
+  // { path: 'dashboard/matrimonial', component: MatrimonialDashboardComponent },
   { path: 'dashboard/social', component: SocialDashboardComponent },
-  { path: 'dashboard/job', component: JobDashboardComponent },
+  // { path: 'dashboard/job', component: JobDashboardComponent },
 ];
 
 @NgModule({
