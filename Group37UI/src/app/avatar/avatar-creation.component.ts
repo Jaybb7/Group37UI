9a83b272ee29b48c1157fc62d368b9a8dd57390e
Taizeen <taizeenmunir18@gmail.com>
2023-10-29 11:05:47 +1100

    finished implementing image generation frontend interface

--- Group37UI/src/app/avatar/avatar-creation.component.ts
+++ Group37UI/src/app/avatar/avatar-creation.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AvatarGenerationService } from './avatar-generation.service';
+import { HttpClient } from '@angular/common/http';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-avatar-dashboard',
@@ -8,6 +11,8 @@ import { AvatarGenerationService } from './avatar-generation.service';
 export class AvatarGenerationServiceComponent implements OnInit {
   userProfile: any;
 
+  apiResponse: any;
+
   constructor(private avatarService: AvatarGenerationService ) { }
 
   ngOnInit(): void {
@@ -15,6 +20,42 @@ export class AvatarGenerationServiceComponent implements OnInit {
     this.avatarService.getUserProfile().subscribe((profile) => {
       this.userProfile = profile;
     });
+    this.apiResponse = '';
+  }
+
+  onUploadButtonClick() {
+    const fileInput = document.getElementById('profile-picture-upload') as HTMLInputElement;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      const selectedFile = fileInput.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        if (event.target == null) {
+          return;
+        }
+        var base64Image = event.target.result as string;
+        base64Image = base64Image.split(',')[1];
+
+        // Make an HTTP GET request to the REST API
+        axios.post('http://localhost:8080/avatar/generate', base64Image, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+            .then(response => {
+              const res = response.data.replace(/\\/g, "");
+                console.log('Avatar generated. Image URL:', res);
+                this.apiResponse = res;
+            })
+            .catch(error => {
+                console.error('Failed to generate avatar:', error);
+            });
+       
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
 
   }
 }
